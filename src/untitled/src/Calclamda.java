d3f96980738626ef4dd20d75cc2defd33f44b6af
chandanv0718 <chandanvwwork@gmail.com>
2026-08-06 21:01:52 +0530

    Delete src directory

--- src/untitled/src/Calclamda.java
+++
@@ -1,9 +0,0 @@
-interface Calculator {
-    int calculate(int a, int b);
-}
-public class Calclamda {
-    public static void main(String[] args) {
-        Calculator div = (int a, int b) -> a / b;
-        System.out.println("Division = " + div.calculate(20, 5));
-    }
-}
--- src/untitled/src/Simplelamdafunction.java
+++
@@ -1,6 +0,0 @@
-public class Simplelamdafunction {
-    public static void main(String[] args) {
-        Runnable hello = () -> System.out.println("Welcome!");
-        hello.run();
-    }
-}
